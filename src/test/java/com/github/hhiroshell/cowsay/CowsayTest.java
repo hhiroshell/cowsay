0c84923ca7c96a9c7065a470780d8978710e6e14
Hiroshi Hayakawa <hhiroshell@gmail.com>
2019-09-09 15:48:32 +0900

    Tests

--- src/test/java/com/github/hhiroshell/cowsay/CowsayTest.java
+++ src/test/java/com/github/hhiroshell/cowsay/CowsayTest.java
@@ -72,273 +72,284 @@ public class CowsayTest {
 		}
 	}
 
-	// /**
-	//  * Test of say method, of class Cowsay with both piped input and arg.
-	//  */
-	// @Test
-	// public void testPipedSayTrumpedByArgs() {
-	// 	try {
-	// 		System.out.println("echo foobar | cowsay Hello");
-	// 		String[] args = new String[]{"Hello"};
-	// 		String expResult = loadExpected("cowsayHello.txt");
-	// 		ByteArrayInputStream in = new ByteArrayInputStream("foobar".getBytes());
-	// 		System.setIn(in);
-	// 		String result = CowsayOrg.say(args);
-	// 		Assert.assertEquals("Message from args trumps piped input", expResult, result);
-	// 	} finally {
-	// 		System.setIn(System.in);
-	// 	}
-	// }
+	/**
+	 * Test of say method, of class Cowsay with both piped input and arg.
+	 */
+	@Test
+	public void testPipedSayTrumpedByArgs() {
+		try {
+			System.out.println("echo foobar | cowsay Hello");
+			String expResult = loadExpected("cowsayHello.txt");
+			String[] args = new String[]{"Hello"};
+			ByteArrayInputStream in = new ByteArrayInputStream("foobar".getBytes());
+			System.setIn(in);
+			Cowsay cowsay = new Cowsay();
+			new CommandLine(cowsay).parseArgs(args);
+			String result = cowsay.say();
+			Assert.assertEquals("Message from args trumps piped input", expResult, result);
+		} finally {
+			System.setIn(System.in);
+		}
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay, with multiple words in multiple args issue #4.
-	//  */
-	// @Test
-	// public void testSayManyWords() {
-	// 	System.out.println("cowsay foo bar baz");
-	// 	String[] args = new String[]{"foo", "bar", "baz"};
-	// 	String expResult = loadExpected("cowsayFooBarBaz.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay, with multiple words in multiple args issue #4.
+	 */
+	@Test
+	public void testSayManyWords() {
+		System.out.println("cowsay foo bar baz");
+		String expResult = loadExpected("cowsayFooBarBaz.txt");
+		String[] args = new String[]{"foo", "bar", "baz"};
+		Cowsay cowsay = new Cowsay();
+		new CommandLine(cowsay).parseArgs(args);
+		String result = cowsay.say();
+		Assert.assertEquals(expResult, result);
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay, with multiple words in one arg.
-	//  */
-	// @Test
-	// public void testSayManyWordsOneArg() {
-	// 	System.out.println("cowsay foo bar baz");
-	// 	String[] args = new String[]{"foo bar baz"};
-	// 	String expResult = loadExpected("cowsayFooBarBaz.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay, with multiple words in one arg.
+	 */
+	@Test
+	public void testSayManyWordsOneArg() {
+		System.out.println("cowsay foo bar baz");
+		String expResult = loadExpected("cowsayFooBarBaz.txt");
+		String[] args = new String[]{"foo bar baz"};
+		Cowsay cowsay = new Cowsay();
+		new CommandLine(cowsay).parseArgs(args);
+		String result = cowsay.say();
+		Assert.assertEquals(expResult, result);
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay, with multiple words piped.
-	//  */
-	// @Test
-	// public void testPipedSayManyWords() {
-	// 	try {
-	// 		System.out.println("echo foo bar baz | cowsay");
-	// 		String expResult = loadExpected("cowsayFooBarBaz.txt");
-	// 		String[] args = new String[0];
-	// 		ByteArrayInputStream in = new ByteArrayInputStream("foo bar baz".getBytes());
-	// 		System.setIn(in);
-	// 		String result = CowsayOrg.say(args);
-	// 		Assert.assertEquals(expResult, result);
-	// 	} finally {
-	// 		System.setIn(System.in);
-	// 	}
-	// }
+	/**
+	 * Test of say method, of class Cowsay, with multiple words piped.
+	 */
+	@Test
+	public void testPipedSayManyWords() {
+		try {
+			System.out.println("echo foo bar baz | cowsay");
+			String expResult = loadExpected("cowsayFooBarBaz.txt");
+			String[] args = new String[0];
+			ByteArrayInputStream in = new ByteArrayInputStream("foo bar baz".getBytes());
+			System.setIn(in);
+			Cowsay cowsay = new Cowsay();
+			new CommandLine(cowsay).parseArgs(args);
+			String result = cowsay.say();
+			Assert.assertEquals(expResult, result);
+		} finally {
+			System.setIn(System.in);
+		}
+	}
 
-	// /**
-	//  * Test non-standard cowthink arg.
-	//  */
-	// @Test
-	// public void testCowthinkArg() {
-	// 	System.out.println("cowsay --cowthink Hello");
-	// 	String[] args = new String[]{"--cowthink", "Hello"};
-	// 	String expResult = loadExpected("cowthinkHello.txt");
-	// 	CowsayCliOrg.addCowthinkOption();
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay.
+	 */
+	@Test
+	public void testSayWrap2() {
+		System.out.println("cowsay -W 2 Hello");
+		String expResult = loadExpected("cowsayWrap2.txt");
+		String[] args = new String[]{"-W", "2", "Hello"};
+		Cowsay cowsay = new Cowsay();
+		new CommandLine(cowsay).parseArgs(args);
+		String result = cowsay.say();
+		Assert.assertEquals(expResult, result);
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay.
-	//  */
-	// @Test
-	// public void testSayWrap2() {
-	// 	System.out.println("cowsay -W 2 Hello");
-	// 	String[] args = new String[]{"-W", "2", "Hello"};
-	// 	String expResult = loadExpected("cowsayWrap2.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay.
+	 */
+	@Test
+	public void testSayNowrap() {
+		System.out.println("cowsay -n msg");
+		String expResult = loadExpected("cowsayLong.txt");
+		String[] args = new String[]{"-n", "Moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo"};
+		Cowsay cowsay = new Cowsay();
+		new CommandLine(cowsay).parseArgs(args);
+		String result = cowsay.say();
+		Assert.assertEquals(expResult, result);
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay.
-	//  */
-	// @Test
-	// public void testSayNowrap() {
-	// 	System.out.println("cowsay -n msg");
-	// 	String[] args = new String[]{"-n", "Moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo"};
-	// 	String expResult = loadExpected("cowsayLong.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay.
+	 */
+	@Test
+	public void testSayWrapZero() {
+		System.out.println("cowsay -W 0 msg");
+		String expResult = loadExpected("cowsayLong.txt");
+		String[] args = new String[]{"-W", "0", "Moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo"};
+		Cowsay cowsay = new Cowsay();
+		new CommandLine(cowsay).parseArgs(args);
+		String result = cowsay.say();
+		Assert.assertEquals(expResult, result);
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay.
-	//  */
-	// @Test
-	// public void testSayWrapZero() {
-	// 	System.out.println("cowsay -W 0 msg");
-	// 	String[] args = new String[]{"-W", "0", "Moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo"};
-	// 	String expResult = loadExpected("cowsayLong.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay with named cow.
+	 */
+	@Test
+	public void testSayWithNamedFile() {
+		System.out.println("cowsay -f tux Hello");
+		String expResult = loadExpected("cowsayTux.txt");
+		String[] args = new String[]{"-f", "tux", "Hello"};
+		Cowsay cowsay = new Cowsay();
+		new CommandLine(cowsay).parseArgs(args);
+		String result = cowsay.say();
+		Assert.assertEquals(expResult, result);
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay with named cow.
-	//  */
-	// @Test
-	// public void testSayWithNamedFile() {
-	// 	System.out.println("cowsay -f tux Hello");
-	// 	String[] args = new String[]{"-f", "tux", "Hello"};
-	// 	String expResult = loadExpected("cowsayTux.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay with named cow - not found.
+	 */
+	@Test
+	public void testSayWithNamedFileNotFound() {
+		System.out.println("cowsay -f abcdefgzzzblah Hello");
+		String expResult = loadExpected("cowsayHello.txt");
+		String[] args = new String[]{"-f", "abcdefgzzzblah", "Hello"};
+		Cowsay cowsay = new Cowsay();
+		new CommandLine(cowsay).parseArgs(args);
+		String result = cowsay.say();
+		Assert.assertEquals(expResult, result);
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay with named cow - not found.
-	//  */
-	// @Test
-	// public void testSayWithNamedFileNotFound() {
-	// 	System.out.println("cowsay -f abcdefgzzzblah Hello");
-	// 	String[] args = new String[]{"-f", "abcdefgzzzblah", "Hello"};
-	// 	String expResult = loadExpected("cowsayHello.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay with modes.
+	 */
+	@Test
+	public void testSayModes() {
+		Set<String> modes = modeMap.keySet();
+		for (String key : modes) {
+			System.out.println("cowsay -" + key + " Hello");
+			String expResult = loadExpected(modeMap.get(key));
+			String[] args = new String[]{'-' + key, "Hello"};
+			Cowsay cowsay = new Cowsay();
+			new CommandLine(cowsay).parseArgs(args);
+			String result = cowsay.say();
+			Assert.assertEquals(expResult, result);
+		}
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay with modes.
-	//  */
-	// @Test
-	// public void testSayModes() {
-	// 	Set<String> modes = modeMap.keySet();
-	// 	for (String key : modes) {
-	// 		String[] args = new String[]{'-' + key, "Hello"};
-	// 		System.out.println("cowsay -" + key + " Hello");
-	// 		String expResult = loadExpected(modeMap.get(key));
-	// 		String result = CowsayOrg.say(args);
-	// 		Assert.assertEquals(expResult, result);
-	// 	}
-	// }
-
-	// /**
-	//  * Test of say method, of class Cowsay, with piped input and flags.
-	//  */
-	// @Test
-	// public void testSayModesPiped() {
-	// 	try {
-	// 		Set<String> modes = modeMap.keySet();
-	// 		for (String key : modes) {
-	// 			String[] args = new String[]{'-' + key};
-	// 			System.out.println("echo Hello | cowsay -" + key);
-	// 			String expResult = loadExpected(modeMap.get(key));
-	// 			ByteArrayInputStream in = new ByteArrayInputStream("Hello".getBytes());
-	// 			System.setIn(in);
-	// 			String result = CowsayOrg.say(args);
-	// 			Assert.assertEquals(expResult, result);
-	// 		}
-	// 	} finally {
-	// 		System.setIn(System.in);
-	// 	}
-	// }
+	/**
+	 * Test of say method, of class Cowsay, with piped input and flags.
+	 */
+	@Test
+	public void testSayModesPiped() {
+		try {
+			Set<String> modes = modeMap.keySet();
+			for (String key : modes) {
+				System.out.println("echo Hello | cowsay -" + key);
+				String expResult = loadExpected(modeMap.get(key));
+				ByteArrayInputStream in = new ByteArrayInputStream("Hello".getBytes());
+				System.setIn(in);
+				String[] args = new String[]{'-' + key};
+				Cowsay cowsay = new Cowsay();
+				new CommandLine(cowsay).parseArgs(args);
+				String result = cowsay.say();
+				Assert.assertEquals(expResult, result);
+			}
+		} finally {
+			System.setIn(System.in);
+		}
+	}
 
 
-	// /**
-	//  * Test of say method, of class Cowsay with custom eyes.
-	//  */
-	// @Test
-	// public void testSayEyes() {
-	// 	System.out.println("-e QQ cowsay");
-	// 	String[] args = new String[]{"-e", "QQ", "Hello"};
-	// 	String expResult = loadExpected("cowsayEyes.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay with custom eyes.
+	 */
+	@Test
+	public void testSayEyes() {
+		System.out.println("-e QQ cowsay");
+		String expResult = loadExpected("cowsayEyes.txt");
+		String[] args = new String[]{"-e", "QQ", "Hello"};
+		Cowsay cowsay = new Cowsay();
+		new CommandLine(cowsay).parseArgs(args);
+		String result = cowsay.say();
+		Assert.assertEquals(expResult, result);
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay with custom eyes.
-	//  */
-	// @Test
-	// public void testSayEyesDollars() {
-	// 	System.out.println("-e $$ cowsay");
-	// 	String[] args = new String[]{"-e", "$$", "Hello"};
-	// 	String expResult = loadExpected("cowsayGreedy.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay with custom eyes.
+	 */
+	@Test
+	public void testSayEyesDollars() {
+		System.out.println("-e $$ cowsay");
+		String expResult = loadExpected("cowsayGreedy.txt");
+		String[] args = new String[]{"-e", "$$", "Hello"};
+		Cowsay cowsay = new Cowsay();
+		new CommandLine(cowsay).parseArgs(args);
+		String result = cowsay.say();
+		Assert.assertEquals(expResult, result);
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay with custom eyes.
-	//  */
-	// @Test
-	// public void testSayEyesBackslash() {
-	// 	System.out.println("-e \\\\ cowsay");
-	// 	String[] args = new String[]{"-e", "\\\\", "Hello"};
-	// 	String expResult = loadExpected("cowsayEyesBackslash.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay with custom eyes.
+	 */
+	@Test
+	public void testSayEyesBackslash() {
+		System.out.println("-e \\\\ cowsay");
+		String expResult = loadExpected("cowsayEyesBackslash.txt");
+		String[] args = new String[]{"-e", "\\\\", "Hello"};
+		Cowsay cowsay = new Cowsay();
+		new CommandLine(cowsay).parseArgs(args);
+		String result = cowsay.say();
+		Assert.assertEquals(expResult, result);
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay with custom tongue.
-	//  */
-	// @Test
-	// public void testSayTongue() {
-	// 	System.out.println("-T V cowsay");
-	// 	String[] args = new String[]{"-T", "V", "Hello"};
-	// 	String expResult = loadExpected("cowsayTongue.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay with custom tongue.
+	 */
+	@Test
+	public void testSayTongue() {
+		System.out.println("-T V cowsay");
+		String expResult = loadExpected("cowsayTongue.txt");
+		String[] args = new String[]{"-T", "V", "Hello"};
+		Cowsay cowsay = new Cowsay();
+		new CommandLine(cowsay).parseArgs(args);
+		String result = cowsay.say();
+		Assert.assertEquals(expResult, result);
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay with custom tongue and eyes.
-	//  */
-	// @Test
-	// public void testSayTongueEyes() {
-	// 	System.out.println("cowsay -T V -e QQ Hello");
-	// 	String[] args = new String[]{"-T", "V", "-e", "QQ", "Hello"};
-	// 	String expResult = loadExpected("cowsayTongueEyes.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay with custom tongue and eyes.
+	 */
+	@Test
+	public void testSayTongueEyes() {
+		System.out.println("cowsay -T V -e QQ Hello");
+		String expResult = loadExpected("cowsayTongueEyes.txt");
+		String[] args = new String[]{"-T", "V", "-e", "QQ", "Hello"};
+		Cowsay cowsay = new Cowsay();
+		new CommandLine(cowsay).parseArgs(args);
+		String result = cowsay.say();
+		Assert.assertEquals(expResult, result);
+	}
 
-	// /**
-	//  * Test of say method, of class Cowsay with long message.
-	//  */
+	/**
+	 * Test of say method, of class Cowsay with long message.
+	 */
 	// @Test
 	// public void testSayLongLine() {
 	// 	System.out.println("cowsay long");
-	// 	String[] args = new String[]{"Moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo"};
 	// 	String expResult = loadExpected("cowsayMulti.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
-
-	// /**
-	//  * Test of think method, of class Cowsay with long message.
-	//  */
-	// @Test
-	// public void testThinkLongLine() {
-	// 	System.out.println("cowthink long");
 	// 	String[] args = new String[]{"Moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo moo"};
-	// 	String expResult = loadExpected("cowthinkMulti.txt");
-	// 	String result = CowsayOrg.think(args);
+	// 	Cowsay cowsay = new Cowsay();
+	// 	new CommandLine(cowsay).parseArgs(args);
+	// 	String result = cowsay.say();
 	// 	Assert.assertEquals(expResult, result);
 	// }
 
-	// /**
-	//  * Test of say method, of class Cowsay with multiline input message.
-	//  */
-	// @Test
-	// public void testSayMultiLineInput() {
-	// 	System.out.println("cowsay mutli");
-	// 	String[] args = new String[]{"This is a test text."
-	// 			+ System.getProperty("line.separator") + "To test long lines in cowsay."
-	// 			+ System.getProperty("line.separator") + "Moo moo moo."};
-	// 	String expResult = loadExpected("cowsayMultiline.txt");
-	// 	String result = CowsayOrg.say(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
+	/**
+	 * Test of say method, of class Cowsay with multiline input message.
+	 */
+	@Test
+	public void testSayMultiLineInput() {
+		System.out.println("cowsay mutli");
+		String expResult = loadExpected("cowsayMultiline.txt");
+		String[] args = new String[]{"This is a test text."
+				+ System.getProperty("line.separator") + "To test long lines in cowsay."
+				+ System.getProperty("line.separator") + "Moo moo moo."};
+		Cowsay cowsay = new Cowsay();
+		new CommandLine(cowsay).parseArgs(args);
+		String result = cowsay.say();
+		Assert.assertEquals(expResult, result);
+	}
 
 	// /**
 	//  * Test of say method, of class Cowsay with multiline input message.
@@ -346,42 +357,34 @@ public class CowsayTest {
 	// @Test
 	// public void testSayMultiLineLongInput() {
 	// 	System.out.println("cowsay mutli long");
+	// 	String expResult = loadExpected("cowsayMultilineLong.txt");
 	// 	String[] args = new String[]{"This is a test text." + System.lineSeparator()
 	// 			+ "To test long lines in cowsay." + System.lineSeparator()
 	// 			+ "This is a really really really really really long line." + System.lineSeparator()
 	// 			+ "Moo moo moo."};
-	// 	String expResult = loadExpected("cowsayMultilineLong.txt");
-	// 	String result = CowsayOrg.say(args);
+	// 	Cowsay cowsay = new Cowsay();
+	// 	new CommandLine(cowsay).parseArgs(args);
+	// 	String result = cowsay.say();
 	// 	Assert.assertEquals(expResult, result);
 	// }
 
-	// /**
-	//  * Test of think method, of class Cowsay.
-	//  */
-	// @Test
-	// public void testThink() {
-	// 	System.out.println("cowthink Hello");
-	// 	String[] args = new String[]{"Hello"};
-	// 	String expResult = loadExpected("cowthinkHello.txt");
-	// 	String result = CowsayOrg.think(args);
-	// 	Assert.assertEquals(expResult, result);
-	// }
-
-	// /**
-	//  * Test parsing all known cowfiles.
-	//  */
-	// @Test
-	// public void testAllCowfiles() {
-	// 	String[] cowfiles = Cowloader.listAllCowfiles();
-	// 	Assert.assertNotEquals(0, cowfiles.length);
-	// 	for (String cowfile : cowfiles) {
-	// 		System.out.println("cowsay -f " + cowfile + " Hello");
-	// 		String[] args = new String[]{"-f", cowfile, "Hello"};
-	// 		String result = CowsayOrg.say(args);
-	// 		Assert.assertNotEquals("", result);
-	// 		System.out.println(result);  // woohoo!
-	// 	}
-	// }
+	/**
+	 * Test parsing all known cowfiles.
+	 */
+	@Test
+	public void testAllCowfiles() {
+		String[] cowfiles = Cowloader.listAllCowfiles();
+		Assert.assertNotEquals(0, cowfiles.length);
+		for (String cowfile : cowfiles) {
+			System.out.println("cowsay -f " + cowfile + " Hello");
+			String[] args = new String[]{"-f", cowfile, "Hello"};
+			Cowsay cowsay = new Cowsay();
+			new CommandLine(cowsay).parseArgs(args);
+			String result = cowsay.say();
+			Assert.assertNotEquals("", result);
+			System.out.println(result);  // woohoo!
+		}
+	}
 
 	/**
 	 * Fetches the expected output.
